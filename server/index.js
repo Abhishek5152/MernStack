284eff516f0467c1667d2895df92ae4e193c5683
Abhishek5152 <Abhishekbhojaviya@gmail.com>
2024-12-11 13:28:26 +0530

    update

--- server/index.js
+++ server/index.js
@@ -15,16 +15,24 @@
 // })
 
 import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
 import path from 'path';
 import {fileURLToPath} from 'url';
 
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const PORT = 3000;
+const PORT = process.env.PORT;
 
 app.get("/",(req, res) =>{
     res.sendfile(path.join(__dirname,"Views","port.html"));
 });
+app.get('/abhi',(res,req) => {
+    res.json({'content':'Hello'});
+})
+
 app.use(express.static(path.join(__dirname,"Views")));
-app.listen(PORT,()=> {console.log(`Server running on http://localhost:${PORT}`)});
+app.listen(PORT,()=> {
+    console.log(`Server running on http://localhost:${PORT} `)
+});
