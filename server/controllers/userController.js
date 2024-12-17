9415d16eab189058a6b71574fcd11b81d63792de
Abhishek5152 <Abhishekbhojaviya@gmail.com>
2024-12-17 14:28:32 +0530

    commit

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -22,3 +22,29 @@ export const lolall = async (req,res)=>{
         res.status(200).json(userData);
     }catch(error){res.status(500).json({err:error});}
 }
+
+export const updateme = async (req,res)=>{
+    try{
+        const {name, email} = req.body;
+        const userData = await User.findOneAndUpdate(
+            {name: name},
+            {$set: {email: email}},
+            {new:true}
+        );
+        if (!userData){
+            res.status(404).json({msg:"Lol user is not here!!"});
+        }
+        res.status(200).json(userData);
+    }catch(error){res.status(500).json({err:error});}
+}
+
+export const deleteme = async (req,res)=>{
+    try{
+        const {email} = req.body;
+        const userData = await User.findOneAndDelete({email: email});
+        if (!userData){
+            return res.status(404).json({msg:"Lol user is not here!!"});
+        }
+        return res.status(200).json({msg:"Deleted",user:userData});
+    }catch(error){return res.status(500).json({err:error});}
+}
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,10 +1,14 @@
 import{createUser} from "../controllers/userController.js";
 import{lolall} from "../controllers/userController.js";
+import{updateme} from "../controllers/userController.js";
+import{deleteme} from "../controllers/userController.js";
 
 import express from "express";
 
 const route = express.Router();
 route.post("/lolall",lolall);
 route.post("/createUser",createUser);
+route.post("/updateme",updateme);
+route.post("/deleteme",deleteme);
 
 export default route;
